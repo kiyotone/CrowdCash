d14a079ae3552339f9b1e08411121af205e6758c
Prateek61 <prateekpoudel61@gmail.com>
2023-02-28 22:02:43 +0545

    Added axios configurations and HOC for authentication
    Added react Higher Order Component withAuth to protect routes and components

---
+++ frontend/src/components/stuff/axios.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { getToken } from './helper';
+
+const baseURL = 'http://localhost:8000';
+
+const api = axios.create({
+    baseURL: baseURL,
+});
+
+api.interceptors.request.use((config) => {
+    const token = getToken();
+    if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+});
+
+export default api;
---
+++ frontend/src/components/stuff/withAuth.js
@@ -0,0 +1,22 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { getToken } from "./helper";
+
+const withAuth = (WrappedComponent) => {
+    const Wrapper = (props) => {
+        const router = useRouter();
+        const token = getToken();
+
+        useEffect(() => {
+            if (!token) {
+                router.push("/login");
+            }
+        }, [token]);
+
+        return <WrappedComponent {...props} />;
+    };
+
+    return Wrapper;
+};
+
+export default withAuth;
