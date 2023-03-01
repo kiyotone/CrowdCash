52a76b75f2ddfc2e1d3b46c87caa52a4b2df2c52
Prateek61 <prateekpoudel61@gmail.com>
2023-03-01 11:08:57 +0545

    Added new django app home

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,4 +20,5 @@ from django.urls.conf import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('userAuthentication.urls')),
+    path('', include('home.urls')),
 ]
