ec912db556a6b26611bcda171d3b19c8589e0ca5
pr-a-sh-ant <prashantaryal07@gmail.com>
2023-03-01 11:17:37 +0545

    Login validation

--- frontend/src/pages/auth/Login.js
+++ frontend/src/pages/auth/Login.js
@@ -1,13 +1,82 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { useRouter } from "next/router";
+import { useState, useReducer, useEffect } from "react";
+import api from "@/components/stuff/axios";
+
+const emailReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    return {
+      value: action.val,
+      isValid: action.val.includes("@"),
+    };
+  }
+
+  if (action.type === "INPUT_BLUR") {
+    return { value: state.value, isValid: state.value.includes("@") };
+  }
+
+  return { value: "", isValid: false };
+};
+
+const passwordReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    return { value: action.val, isValid: action.val.trim().length > 6 };
+  }
+  if (action.type === "INPUT_BLUR") {
+    return { value: state.value, isValid: state.value.trim().length > 6 };
+  }
+  return { value: "", isValid: false };
+};
 
 const Login = () => {
   const main = useSelector((state) => state.main);
   const router = useRouter();
-  const isDisabled = false;
 
-  const border_color = "#39FF14";
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: "",
+    isValid: null,
+  });
+
+  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
+    value: "",
+    isValid: null,
+  });
+
+  const { isValid: emailIsValid } = emailState;
+  const { isValid: passwordIsValid } = passwordState;
+
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      setFormIsValid(emailIsValid && passwordIsValid);
+    }, 500);
+
+    return () => {
+      clearTimeout(identifier);
+    };
+  }, [emailIsValid, passwordIsValid]);
+  const emailChangeHandler = (event) => {
+    dispatchEmail({ type: "USER_INPUT", val: event.target.value });
+  };
+
+  const passwordChangeHandler = (event) => {
+    dispatchPassword({ type: "USER_INPUT", val: event.target.value });
+  };
+
+  const validateEmailHandler = () => {
+    dispatchEmail({ type: "INPUT_BLUR" });
+  };
+
+  const validatePasswordHandler = () => {
+    dispatchEmail({ type: "INPUT_BLUR" });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log(emailState.value, passwordState.value);
+  };
 
   return (
     <div className="flex bg-background_color overflow-hidden h-screen w-screen fixed top-0 left-0 items-center justify-center">
@@ -15,23 +84,32 @@ const Login = () => {
         <div className="m-4 text-[2rem] font-bold text-primary">Join US</div>
 
         <div className="mt-2 w-[24rem] h-[1rem]">
-          <hr className={` m-3 bg-${border_color}`} />
+          <hr className={` m-3 bg-[#eee]`} />
         </div>
 
-        <div className="mt-3 flex flex-col items-center">
+        <form
+          className="mt-3 flex flex-col items-center"
+          onClick={submitHandler}
+        >
           <input
             type="text"
             placeholder="Email or Username"
             className={`pl-4 mt-4 bg-transparent border-2 rounded-md w-[19rem] h-[3rem] border-[#555] outline-1 outline-secondary`}
+            value={emailState.value}
+            onChange={emailChangeHandler}
+            onBlur={validateEmailHandler}
           />
           <input
             type="password"
             placeholder="Password"
             className={`pl-4 mt-4 bg-transparent border-2 rounded-md outline-secondary w-[19rem] h-[3rem] border-[#555]`}
+            value={passwordState.value}
+            onChange={passwordChangeHandler}
+            onBlur={validatePasswordHandler}
           />
           <button
             className={`pl-4 mt-4 rounded-md w-[19rem] h-[3rem] button bg-button_secondary disabled:bg-gray-400 disabled:cursor-not-allowed text-[#fff]`}
-            disabled={isDisabled}
+            disabled={!formIsValid}
           >
             Continue
           </button>
@@ -44,7 +122,7 @@ const Login = () => {
                 type="checkbox"
                 id="remember"
                 placeholder="Password"
-                className={`mr-2 bg-transparent border-2 rounded-md border-[${border_color}] w-[1rem] h-[1rem]`}
+                className={`mr-2 bg-transparent border-2 rounded-md border-[#fff] w-[1rem] h-[1rem]`}
               />
               <label htmlFor="remember"> Remember Me</label>
             </div>
@@ -65,7 +143,7 @@ const Login = () => {
               Join now
             </div>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
