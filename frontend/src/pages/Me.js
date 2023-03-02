66782d23344470528d20874f491dee707efc4264
kiyotone <kunwarkirtan36@gmail.com>
2023-03-02 10:39:35 +0545

    mebackend connected

--- frontend/src/pages/Me.js
+++ frontend/src/pages/Me.js
@@ -1,7 +1,28 @@
+import api from '@/components/stuff/axios'
 import withAuth from '@/components/stuff/withAuth'
-import React from 'react'
+import React, { useState } from 'react'
 
 function Me() {
+
+    const [loadRequests,setLoanRequests] = useState([])
+    const [investmentRequests,setInvestmentRequests] = useState([])
+
+    const getUserLoans = async () =>{
+        const response = await api.get('/myrequests')
+        console.log(response)
+        setLoanRequests(response.data.loan_requests)
+        setInvestmentRequests(response.data.investment_requests)
+    }
+
+    useEffect(() => {
+        
+        getUserLoans();
+        
+
+      
+    }, [])
+    
+
   return (
     <div className=''>
         
