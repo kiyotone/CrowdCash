4c81e7c4b4a3a4f17244dcd1e2d37002b9a367ad
pr-a-sh-ant <prashantaryal07@gmail.com>
2023-03-02 06:21:59 +0545

    Deals

--- frontend/src/components/topbar/Deals/Deals.js
+++ frontend/src/components/topbar/Deals/Deals.js
@@ -1,37 +1,26 @@
-import React from 'react'
+import React from "react";
 
 export default function Deals(props) {
-    const data = props.data
-  
+  const data = props.data;
 
   return (
-    
-    <div  className='h-20 flex flex-col text-sm bg-slate-800 border-[.001rem]'>
-        
-        <div className='flex items-center '>
-
-            {data.lender.firstname} {' '} {data.lender.lastname} {'>'} {data.borrower.firstname} {' '} {data.borrower.lastname}   
-
+    <div className="h-20 flex flex-col text-sm bg-slate-800 border-[.001rem]">
+      <div className="flex items-center justify-between">
+        <div className="text-[#2c8b35]">
+          {data.lender.firstname} {data.lender.lastname}
         </div>
-
-        <div className=''>
-          Rs {data.amount} {'>'} Rs {data.finalAmount}
+        <div className="text-[#b84f4f]">
+          {data.borrower.firstname} {data.borrower.lastname}
         </div>
+      </div>
 
-        <div> 
-          {data.weeks} weeks time
-        </div>
-        <div >
-          
-        </div>
-        
-            
-            
-            
-          
-        </div>
-    
+      <div className="flex items-center justify-between">
+        <div className="text-[#2c8b35]">Rs {data.amount}</div>
+        <div className="text-[#b84f4f]">Rs {data.finalAmount}</div>
+      </div>
 
-)
-
+      <div>{data.weeks} weeks time</div>
+      <div></div>
+    </div>
+  );
 }
--- frontend/src/components/topbar/Deals/DealsBar.js
+++ frontend/src/components/topbar/Deals/DealsBar.js
@@ -1,57 +1,60 @@
-import React, { useState } from 'react'
-import { useSelector } from 'react-redux';
-import Deals from './Deals';
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import Deals from "./Deals";
 
 function DealsBar() {
-
-  const user = useSelector((state)=> state.user)
-  const [state,setState] = useState("borrow");
-  const [dealList,setDealList] = useState(user.borrows);
-  console.log(dealList)
+  const user = useSelector((state) => state.user);
+  const [state, setState] = useState("borrow");
+  const [dealList, setDealList] = useState(user.borrows);
+  console.log(dealList);
 
   const changeDeals = (deals) => {
-      if (deals == "borrow"){
-        setState("borrow")
-        setDealList(user.borrows)
-      }
-      else if (deals == "lend"){
-        setState("lend")
-        setDealList(user.lends)
-      }
-  }
+    if (deals == "borrow") {
+      setState("borrow");
+      setDealList(user.borrows);
+    } else if (deals == "lend") {
+      setState("lend");
+      setDealList(user.lends);
+    }
+  };
 
   return (
-   
-   <div className='w-96 h-[30rem] top-14 right-[8.9rem] absolute rounded-md text-white bg-gray-800 overflow-y-scroll'>
-  
-          <div className='flex m-1 items-center justify-center h-10 border-gray-400 border-[3px] rounded-md'> Deals </div>
-          <div className='flex h-10'>
-  
-          <div onClick={()=>changeDeals("borrow") } className={`border-gray-400 w-[50%] border-[3px] rounded-t-md ${ state=="borrow" ? "border-b-0": ""}`} >
-              <div className='flex flex-col items-center h-10'>
-              Borrows
-              </div>
-          </div>
-          <div onClick={()=> changeDeals("lend")} className={`border-gray-400 w-[50%] border-[3px] rounded-t-md ${ state=="lend" ? "border-b-0": ""}`} >
-          <div className='flex flex-col items-center h-10'>
-              Lends
-          </div>
-
-          </div>
-
-          </div>
-          <div className='mt-3'>
-            {dealList && 
-              dealList.map((ele) => (
-                <Deals data={ele}/>
-              ))
-            }
-
-          </div>
-
+    <div className="w-96 h-[30rem] top-14 right-[8.9rem] absolute rounded-md text-white bg-gray-800">
+      <div className="flex m-1 items-center justify-center h-10 rounded-md transition-all ">
+        {" "}
+        Deals{" "}
       </div>
-   
-  )
+      <div className="flex h-10 gap-1 px-2">
+        <div
+          onClick={() => changeDeals("borrow")}
+          className={`bg-[#b84f4f] w-[50%]  rounded-md ease-in-out duration-200 ${
+            state == "borrow"
+              ? "-translate-y-1 shadow-lg shadow-[rgba(0,0,0,0.5)]"
+              : ""
+          }`}
+        >
+          <div className="flex flex-col items-center justify-center h-10 ">
+            Borrows
+          </div>
+        </div>
+        <div
+          onClick={() => changeDeals("lend")}
+          className={`bg-[#2c8b35] w-[50%]  rounded-md ease-in-out duration-200 ${
+            state == "lend"
+              ? "-translate-y-1 shadow-lg shadow-[rgba(0,0,0,0.5)]"
+              : ""
+          }`}
+        >
+          <div className="flex flex-col items-center h-10 justify-center">
+            Lends
+          </div>
+        </div>
+      </div>
+      <div className="mt-3 overflow-y-scroll">
+        {dealList && dealList.map((ele) => <Deals data={ele} />)}
+      </div>
+    </div>
+  );
 }
 
-export default DealsBar
+export default DealsBar;
