7860b460c172dd15d8aef93ad223ba9b0389b7b2
kiyotone <kunwarkirtan36@gmail.com>
2023-03-02 10:33:00 +0545

    me

---
+++ frontend/src/pages/Me.js
@@ -0,0 +1,15 @@
+import withAuth from '@/components/stuff/withAuth'
+import React from 'react'
+
+function Me() {
+  return (
+    <div className=''>
+        
+
+
+
+    </div>
+  )
+}
+
+export default withAuth(Me)
